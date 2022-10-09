d6f9f192c5a092157f8b02328b5c304d2b3a14c6
Phil Pinsdorf <philemon.pinsdorf@live.de>
2022-10-10 01:13:17 +0200

    Backend and Middleware works 100%

--- background/background.js
+++ background/background.js
@@ -1,7 +1,24 @@
+/*
+Ablauf:
+ - Bei Start: Lade die letzten 10 Nachichten in ein JSON Objekt, dass dann immer angezeigt wird, wenn das Popup geöffnet wird. DONE
+ - Bei Hook: Füge Nachicht in JSON Objekt hinzu und aktualisier Tabelle in Popup 
+ - Bei Send: Füge Nachicht in JSON Objekt hinzu, pushe in Database und aktualisier Tabelle in Popup
+*/
+
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
-import { getFirestore, addDoc, collection, getDocs, query, orderBy, limit, onSnapshot, where, Timestamp  } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js'
+import { getFirestore, addDoc, collection, query, onSnapshot, Timestamp, where } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js'
 
+// Vars
+const start_timestamp = Timestamp.now();
+let message_cache = []
+let msgPort;
+let canMessage;
+let unreadMessages = 0;
+
+//Api Keys
 const firebaseConfig = {
     apiKey: "AIzaSyA0RO5Yujusyw8T1ypBapibhs-qftyw_Tg",
     authDomain: "fh-aachen-chat-room.firebaseapp.com",
@@ -12,54 +29,94 @@ const firebaseConfig = {
     measurementId: "G-4Y704HZKYZ"
 };
 
-var message_cache = []
+// Set Badge Color
+chrome.action.setBadgeBackgroundColor({color: 'red'});
 
 // Initialize Firebase and Database
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const c = collection(db, "messages");  
+const q = query(c, where("date", ">", start_timestamp));
 
-var c = collection(db, "messages")
-var q = query(c, orderBy("date", "desc"), limit(1));
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.type === "getMessageCache")
-      sendResponse({messageCache: message_cache});
-    if (request.type === "sendMessage") {
-      console.log("!!!!!!!!!!!!!!!!!!!!!")
-      addDoc(c, {
-        user: request.message.user,
-        message: request.message.text,
-        date: Timestamp.now()
-      }).then(res => {
-        console.log("Document written with ID: ", res.id);
-      });
-    }
-  }
-);
-
-const unsubscribe = onSnapshot(q, (snapshot) => {
+// Listen for Updates from Database
+onSnapshot(q, (snapshot) => {
   snapshot.docChanges().forEach((change) => {
-    if (change.type === "added") {
-      var message = {};
-      message.user = change.doc.data().user;
-      message.text = change.doc.data().message;
-      message.date = change.doc.data().date;
-      
-      message_cache.push(message);
-      sendMessageToPopup(message);
-    }
+    if (change.type === "added") 
+      addMessage(change.doc.data());
   });
 });
 
-function sendMessageToPopup(message) {
-  chrome.runtime.sendMessage({type: "incomingMessage", message: message}, (res) => {});
+// Upload Message to Firestore
+function uploadMessage(message) {
+  addDoc(c, {
+    user: message.user,
+    message: message.text,
+    date: Timestamp.now()
+  }).then(res => {
+    console.log("Document written with ID: ", res.id);
+  });
 }
 
-/*
-Ablauf:
- - Bei Start: Lade die letzten 10 Nachichten in ein JSON Objekt, dass dann immer angezeigt wird, wenn das Popup geöffnet wird. DONE
- - Bei Hook: Füge Nachicht in JSON Objekt hinzu und aktualisier Tabelle in Popup 
- - Bei Send: Füge Nachicht in JSON Objekt hinzu, pushe in Database und aktualisier Tabelle in Popup
-*/
 
+
+// Connect to Popup once its opened and close connection, when Popup is closed
+chrome.runtime.onConnect.addListener(function(port) {
+  if (port.name === "popup") {
+    msgPort = port;
+    canMessage = true;
+    removeUnread();
+
+    // Message Listener
+    msgPort.onMessage.addListener(function(packet) {
+      switch (packet.type) {
+        // Sending Back Message Cache
+        case "getMessageCache": 
+          msgPort.postMessage({type: "receiveMessageCache", messageCache: message_cache});
+          break;
+
+        // Send Message
+        case "sendMessage":
+          uploadMessage(packet.message);
+          break;
+      }
+    });
+
+    // Disconnect Lstener
+    msgPort.onDisconnect.addListener(function() {
+      canMessage = false;
+    });
+  }
+});
+
+// Add Message to Cache and send it to Popup
+function addMessage(data){
+  const message = {
+    user: data.user,
+    text: data.message,
+    date: data.date
+  };
+  
+  message_cache.push(message);
+
+  if (canMessage) {
+    msgPort.postMessage({type: "receiveMessage", message: message});
+  } else {
+    increaseUnread();
+  }
+}
+
+function increaseUnread() {
+  unreadMessages += 1;
+
+  if (unreadMessages >= 10) {
+    chrome.action.setBadgeText({text: "10+"}); 
+  } else {
+    chrome.action.setBadgeText({text: unreadMessages + ""}); 
+  }
+    
+}
+
+function removeUnread() {
+  unreadMessages = 0;
+  chrome.action.setBadgeText({text: ""}); 
+}
--- popup/popup.js
+++ popup/popup.js
@@ -1,39 +1,63 @@
+// Dom Elements that need to be manipulated
 var message_container = document.getElementById("messages"); 
 var login_button = document.getElementById("send-button-login"); 
 var text_input = document.getElementById("textinput"); 
 var send_button = document.getElementById("send-button"); 
 
 var username;
-
 getUsername();
-loadMessageCache();
 
 
-// Request Messages thet are Cached in the Background Script
-function loadMessageCache() {
-    chrome.runtime.sendMessage({type: "getMessageCache"}, function(response) {
-        response.messageCache.forEach(element => {
-            addMessage(element);
-        });
-    });
+// Open Port to Backend
+const port = chrome.runtime.connect({ name: "popup" });
+
+// Listen for Messages from Backend
+port.onMessage.addListener(function(packet) {
+    switch (packet.type) {
+        // Receive Message Cache from Backend
+        case "receiveMessageCache":
+            packet.messageCache.forEach(element => {addMessage(element)});
+            break;
+        
+        // Receive Message from Backend
+        case "receiveMessage":
+            addMessage(packet.message);
+            break;
+    }
+});
+
+// Requesting Mesage Cache
+port.postMessage({type: "getMessageCache"});
+
+// When the Send Button is Clicked Send Message back to Background Script
+send_button.addEventListener('click', function() {
+    port.postMessage({type: "sendMessage", message: {user: username, text: text_input.value}});
+    text_input.value = "";
+});
+
+// Add a Message to the messages div
+function addMessage(message) {
+    message_container.innerHTML += message.user + ": " + message.text + "<br>"
 }
 
+
+
+// Needs to be redone
 
 // Get the Username from the Chrome Storage
 function getUsername() {
     chrome.storage.local.get(['user'], function(result) {
         console.log(result.user)
         username = result.user;
-        showLoginScreen();
+        shouldShowLoginScreen();
     });
 }
 
 
 // If the User already has a Username show the chat Screen else show the Login Screen
-function showLoginScreen() {
+function shouldShowLoginScreen() {
     console.log(username)
     if(username != undefined) {
-        console.log("2")
         document.getElementById("login").style.display = "none";
         document.getElementById("chat").style.display = "inline";
     }
@@ -48,28 +72,11 @@ login_button.addEventListener('click', function() {
     chrome.storage.local.set({user: new_username}, function() {
         console.log('Saved Username: ' + new_username);
         username = new_username;
-        showLoginScreen();
+        shouldShowLoginScreen();
     });
 });
 
 
-// When the Send Button is Clicked Send Message back to Background Script
-send_button.addEventListener('click', function() {
-    chrome.runtime.sendMessage({type: "sendMessage", message: {user: username, text: text_input.value}}, function(response) {
-        text_input.value = "";
-    });
-});
 
 
-// Listen for incoming Messages
-chrome.runtime.onMessage.addListener(function(message, messageSender, sendResponse) {
-    if (message.type === "incomingMessage") {
-        addMessage(message.message);
-    }
-});
 
-
-// Add a Message to the messages div
-function addMessage(message) {
-    message_container.innerHTML += message.user + ": " + message.text + "<br>"
-}
